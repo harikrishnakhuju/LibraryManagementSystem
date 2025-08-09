fdc5172037a356dab55324df341871d3dda087cb
harikrishnakhuju <krishnakhuju707@gmail.com>
2025-08-09 21:27:15 +0545

    return book extra

--- routes/console.php
+++ routes/console.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
@@ -7,4 +8,6 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('app:check-overdue-books')->daily();
+return function (Schedule $schedule) {
+    $schedule->command('app:check-overdue-books')->daily();
+};
